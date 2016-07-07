9710029b59e03da86ae81af8bc4cebd964f967c7
Evgeny <ev.kravcov@gmail.com>
2016-07-08 00:42:32 +0800

    перенос файлов в отдельную папку

---
+++ school-php/school-php-lesson4.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * FUNCTION
+ * ФУНКЦИЯ
+ *
+ */
+
+function calc ($num1, $num2, $action){
+    $action = $num1 + $num2;
+    echo $action;
+    return $action;
+
+}
+calc(5,5, $sum);
+echo $action;
