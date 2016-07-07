a292f95fc15d5bdc43555fde3912f3967255e2d3
Evgeny <ev.kravcov@gmail.com>
2016-07-07 08:56:43 +0800

    Разбор теории

--- lesson 2/school-php-lesson2.php
+++ lesson 2/school-php-lesson2.php
@@ -65,3 +65,92 @@ $var5 = '';
 if (!empty($var5)) {
     echo 'Данная переменная НЕ пустая';
 }
+?>
+<br>
+<br>
+<br>
+<?php
+/**
+ * ЦИКЛЫ
+ * - while
+ * - do-while
+ * - for
+ * - foreach (для массивов)
+ */
+//WHILE
+$x = 1;
+echo $x . 'Текст до цикла <br>';
+while($x == 1 || $x ==2 || $x ==3) {
+    echo 'Произвольный текст <br>';
+    ++$x; // Увеличиваем $x на 1
+    echo $x . '<br>';
+}
+echo 'Текст после цикла <br>';
+?>
+    <br>
+    <br>
+    <br>
+<?php
+$x = 1;
+echo 'Текст до цикла <br>';
+while($x < 5) {
+    echo $x .') Произвольный текст <br>';
+    ++$x; // Увеличиваем $x на 1
+}
+echo 'Текст после цикла <br>';
+?>
+    <br>
+    <br>
+    <br>
+<?php
+//DO-WHILE
+echo 'Перед циклом<br>';
+$x = 10;
+do {
+    echo $x . '<br>';
+    ++$x;
+} while ($x < 10);
+echo 'После цикла';
+?>
+    <br>
+    <br>
+    <br>
+<?php
+//FOR
+echo 'Перед цикла <br>';
+
+for($x = 1; $x < 10; ++$x) {
+    echo $x . '<br>';
+}
+echo 'После цикла';
+?>
+    <br>
+    <br>
+    <br>
+<?php
+//FOREACH
+$names = array('Вася','Петя','Влад','Женя');
+echo '<pre>' . print_r ($names,1) . '</pre>';
+    echo 'В нашем доме живут: ';
+foreach ($names as $key => $value) {
+    if ($value == 'Влад') {
+        break; // обрывает цикл
+    }
+    echo $value . ', ';
+};
+echo '<br>Цикл завершился';
+?>
+    <br>
+    <br>
+    <br>
+<?php
+// continie;
+$names = array('Вася','Петя','Влад','Женя');
+echo '<pre>' . print_r ($names,1) . '</pre>';
+echo 'В нашем доме живут: ';
+foreach ($names as $key => $value) {
+    if ($value == 'Влад') {
+        continue; //Завершить интерацию, то есть не выводить, но дальше будет обрабатываться
+    }
+    echo $value . ', ';
+};
--- lesson 3/homework repeat/homework_1.php
+++ lesson 3/homework repeat/homework_1.php
@@ -10,39 +10,33 @@
 /**
  * Массив - Меню
  */
-$MENU = [
-    [
+$MENU = [];
+    $MENU[0] = [
         'menutext' => 'Главная',
         'description' => 'Главная страница'
-    ],
-    [
+    ];
+    $MENU[1] = [
         'menutext' => 'Каталог',
         'description' => 'Ассортимент товаров'
-    ],
-    [
+    ];
+    $MENU[2] = [
         'menutext' => 'Контакты',
         'description' => 'Наши координаты'
-    ],
-    [
+    ];
+    $MENU[3] = [
         'menutext' => 'Отзывы',
         'description' => 'Отзывы покупателей'
-    ],
-    [
+    ];
+    $MENU[4] = [
         'menutext' => 'Помощь',
         'description' => 'Центр помощи'
-    ],
-];
+    ];
+
+$position = isset($_GET['page']) ? $_GET['page'] : 'null';
 
 foreach ($MENU as $key => $value)
 {
-    foreach ($value as $href => $text){
-            if($text) {
-                echo "<p>Здесь {$text['menutext']}</p>";
-            }
-        }
-    ?>
-
-<?php
+    echo '<a href="  $MENU[$key]['menutext'] . '<br>';
 }
 ?>
 </body>
